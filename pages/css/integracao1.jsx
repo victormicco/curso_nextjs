d1df625ab543e342ffdaeb2b5a932083f861fa29
victormicco <victor.oficial093@gmail.com>
2022-04-04 22:58:05 -0300

    Global CSS implementation

---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,9 @@
+export default function Integracao() {
+  return (
+    <>
+      <div className="vermelha">Texto #01</div>
+      <div className="azul">Texto #02</div>
+      <div className="branca">Texto #03</div>
+    </>
+  );
+}
