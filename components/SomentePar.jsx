ec0537c081cc4b20374281555e6bd61b769bda94
victormicco <victor.oficial093@gmail.com>
2022-04-06 01:11:04 -0300

    props children

---
+++ components/SomentePar.jsx
@@ -0,0 +1,12 @@
+export default function SomentePar(props) {
+  const numeroPar = props.numero % 2 === 0;
+
+  return numeroPar ? <h1>{props.numero}</h1> : null;
+}
+
+/*if (props.numero % 2 === 0) {
+  return <h1>{props.numero}</h1>;
+} else {
+  return null;
+}
+*/
