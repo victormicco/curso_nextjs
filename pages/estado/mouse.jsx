a5df1eb3bb625cd4833bf8ad316f534fa9314c6a
victormicco <victor.oficial093@gmail.com>
2022-04-12 22:26:00 -0300

    Hook useState introduction

---
+++ pages/estado/mouse.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function Mouse() {
+  const arrayX = useState(0);
+  let x = arrayX[0];
+  const alterarX = arrayX[1];
+
+  const arrayY = useState(0);
+  let y = arrayY[0];
+  const alterarY = arrayY[1];
+
+  const estilo = {
+    flexDirection: "column",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#222",
+    color: "#fff",
+    height: "100vh",
+    fontSize: "60px",
+  };
+
+  function quandoMover(ev) {
+    alterarX(ev.pageX);
+    alterarY(ev.pageY);
+    console.log(alterarX, alterarY);
+  }
+  return (
+    <div style={estilo} onMouseMove={quandoMover}>
+      <h1>Onde seu mouse está?</h1>
+      <span>Eixo X: {x} </span>
+      <span>Eixo y: {y} </span>
+    </div>
+  );
+}
