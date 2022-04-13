d636f72ac85906b12ab9422229ddc9174e701a58
victormicco <victor.oficial093@gmail.com>
2022-04-12 23:26:44 -0300

    Controled Component

---
+++ pages/estado/formulario.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+export default function Formulario() {
+  const [valor, setValor] = useState("Inicial");
+
+  function alterarInput() {
+    setValor(valor + "!");
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <span>{valor}</span>
+      <input
+        type="text"
+        value={valor}
+        onChange={(e) => setValor(e.target.value)}
+      />
+      <button onClick={alterarInput}>Alterar</button>
+    </div>
+  );
+}
