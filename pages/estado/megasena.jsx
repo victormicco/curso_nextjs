f3e4ab64a95b5796f5bae0447926c9df46ff89a9
victormicco <victor.oficial093@gmail.com>
2022-04-28 20:55:14 -0300

    megasena finished

--- pages/estado/megasena.jsx
+++ pages/estado/megasena.jsx
@@ -1,9 +1,14 @@
 import NumeroDisplay from "../../components/NumeroDisplay";
-import { mega } from "../../functions/mega";
-import { useState } from "react";
+import { Mega } from "../../functions/Mega";
+import { useState, useEffect } from "react";
 
 export default function Megasena() {
-  const [numeros, setNumeros] = useState(mega());
+  const [qtde, setQtde] = useState(6);
+  const [numeros, setNumeros] = useState([]);
+
+  useEffect(() => {
+    setNumeros(Mega());
+  }, []);
 
   function renderNumeros() {
     return numeros.map((numero) => (
@@ -12,9 +17,33 @@ export default function Megasena() {
   }
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
       <h1>Mega Sena</h1>
-      <div>{renderNumeros()}</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        {renderNumeros()}
+      </div>
+      <div>
+        <input
+          type="number"
+          min={6}
+          max={20}
+          value={qtde}
+          onChange={(ev) => setQtde(+ev.target.value)}
+        />
+        <button onClick={() => setNumeros(Mega(qtde))}>Gerar Aposta</button>
+      </div>
     </div>
   );
 }
